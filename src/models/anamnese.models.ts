83e624b11b9bb631d5a6c216cc1bb93e73feb388
francisco-duo <francisco.duo2000@gmail.com>
2024-12-04 22:23:14 -0300

    feat: update user model

--- src/models/anamnese.models.ts
+++ src/models/anamnese.models.ts
@@ -1,18 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
 import { Psychologist } from "./user.models";
+import { Patient } from "./user.models";
 
 
 @Entity("anamnese_adulto")
 export class AnamneseAdulto {
     @PrimaryGeneratedColumn()
     id!: number;
-  
+
     @Column({ type: 'text' })
     description!: string;
-  
+
     @Column({ type: 'date' })
     date!: Date;
-  
+
+    // Relação ManyToOne com o modelo de Paciente
+    @ManyToOne(() => Patient, patient => patient.id, { eager: true })
+    @JoinColumn({ name: "patient_id" })
+    patient!: Patient;
+
     // Relação ManyToOne com o modelo de Psicologo
     @ManyToOne(() => Psychologist, psicologo => psicologo.id, { eager: true })
     @JoinColumn({ name: 'psicologo_id' })
